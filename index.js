b73f3ec3dea2d60463390282b439c387aefa938c
DavidZhang <strucoder@DavidZhangs-MacBook-Pro.local>
2016-01-14 19:39:53 +0800

    fix

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-module.exports = require('./lib/llog');
+'use strict'
+
+exports.getLogger = function(name) {
+  const pid = process.pid;
+  return {
+    info: function() {
+      let args = [].slice.call(arguments);
+      args.push(name, 'info ', pid);
+      log.apply(null, args);
+
+    },
+    error: function() {
+      let args = [].slice.call(arguments);
+      args.push(name, 'error', pid);
+      log.apply(null, args);
+    },
+    warn: function() {
+      let args = [].slice.call(arguments);
+      args.push(name, 'warn', pid);
+      log.apply(null, args);
+    },
+    fatal: function() {
+      let args = [].slice.call(arguments);
+      args.push(name, 'fatal', pid);
+      log.apply(null, args);
+    },
+    debug: function(){
+      let args = [].slice.call(arguments);
+      args.push(name, 'debug', pid);
+      log.apply(null, args);
+    }
+  }
+}
