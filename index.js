ed00e515d5abe8703b403c104dc708e9a02a58d2
strucoder <zdwloveschina@163.com>
2016-06-19 17:36:01 +0800

    for better log

--- index.js
+++ index.js
@@ -1,35 +1,55 @@
 'use strict';
 
-const log = require('util').log;
+const colors = require('colors');
+
+colors.setTheme({
+  silly: 'rainbow',
+  input: 'grey',
+  verbose: 'cyan',
+  prompt: 'grey',
+  info: 'green',
+  data: 'grey',
+  help: 'cyan',
+  warn: 'yellow',
+  debug: 'blue',
+  error: 'red'
+});
+
 
 exports.getLogger = function(name) {
   const pid = process.pid;
+  function log() {
+    let args = [].slice.call(arguments);
+    let currentTime = new Date();
+    let time = `${currentTime.getFullYear()}-${currentTime.getMonth() + 1}-${currentTime.getDate()} ${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}`;
+    console.log(`${time} - ${args.join(' ')}`);
+  }
   return {
     info: function() {
+      let _log = [colors.info('info '), name, pid];
       let args = [].slice.call(arguments);
-      args.push(name, 'info ', pid);
-      log.apply(null, args);
+      log.apply(null, _log.concat(args));
 
     },
     error: function() {
+      let _log = [colors.error('error'), name, pid];
       let args = [].slice.call(arguments);
-      args.push(name, 'error', pid);
-      log.apply(null, args);
+      log.apply(null, _log.concat(args));
     },
     warn: function() {
+      let _log = [colors.warn('warn'), name, pid];
       let args = [].slice.call(arguments);
-      args.push(name, 'warn', pid);
-      log.apply(null, args);
+      log.apply(null, _log.concat(args));
     },
     fatal: function() {
+      let _log = [colors.warn('fatal'), name, pid];
       let args = [].slice.call(arguments);
-      args.push(name, 'fatal', pid);
-      log.apply(null, args);
+      log.apply(null, _log.concat(args));
     },
     debug: function(){
+      let _log = [colors.debug('debug'), name, pid];
       let args = [].slice.call(arguments);
-      args.push(name, 'debug', pid);
-      log.apply(null, args);
+      log.apply(null, _log.concat(args));
     }
   }
 }
